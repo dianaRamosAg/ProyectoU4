fd534a18f596981ea532ab5300e3a5ebaaced8ac
dianaRamosAg <52227360+dianaRamosAg@users.noreply.github.com>
2019-07-25 16:39:36 -0500

    PRUEBA RECIPES

---
+++ src/app/recipe-list/recipe-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { RecipesService} from "../recipes.service"
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  public recipe;
+
+  constructor(private apirecipe:RecipesService) { 
+    this.apirecipe.getRecipe().subscribe(
+      (recipe_result)=>{
+        this.recipe = recipe_result;
+        console.log(this.recipe);
+      },
+      (err)=>{
+        console.log(err);
+        this.recipe = err;
+      }
+    );
+  }
+  
+
+  ngOnInit() {
+  }
+
+}
