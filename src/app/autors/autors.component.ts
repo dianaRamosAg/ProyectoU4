423b08ef58722d83cb38955a695fd012934fb9e5
Mona Lisa <fernando_eh@outlook.com>
2019-07-26 07:40:28 -0600

    Agregar nuevos autores

--- src/app/autors/autors.component.ts
+++ src/app/autors/autors.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AutorsService } from '../autors.service';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-autors',
@@ -8,9 +9,10 @@ import { AutorsService } from '../autors.service';
 })
 export class AutorsComponent implements OnInit {
   public authors;
+  formAuthor;
 
   constructor(
-    private apiauthor: AutorsService) {
+    private apiauthor: AutorsService, private formBuilder:FormBuilder) {
       this.apiauthor.getAuthors().subscribe(
         (author_result) =>{
           this.authors = author_result;
@@ -19,6 +21,38 @@ export class AutorsComponent implements OnInit {
         (err) => {
           console.log(err);
           this.authors = err;
+        }
+      );
+
+      this.createForm();
+    }
+
+    createForm(){
+      this.formAuthor = this.formBuilder.group({
+        name:['',[Validators.required,Validators.minLength(4)]],
+        email:['',[Validators.required,Validators.minLength(4)]]
+      });
+    }
+
+    guardarAutor(authorToSave) {
+      console.log(authorToSave);
+      this.apiauthor.addAuthor(authorToSave)
+      .subscribe(
+        (prod)=> {
+          this.apiauthor.getAuthors().subscribe(
+            (author_result)=>{
+              this.authors = author_result;
+              console.log(this.authors);
+            },
+            (err)=>{
+              console.log(err);
+              this.authors = err;
+            }
+          );
+          this.createForm();
+        },
+        (err)=>{
+          console.log(err);
         }
       );
     }
