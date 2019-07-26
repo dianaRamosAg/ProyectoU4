4dfee85577a3719018d7f45c2e5214e88813df21
MiguelGarcia <miangarciago@ittepic.edu.mx>
2019-07-25 23:39:16 -0600

    cambios menores

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,9 @@ import { RecipesDetailComponent } from "./recipes-detail/recipes-detail.componen
 
 const routes: Routes = [
   {path: 'author', component: AutorsComponent},
-  {path: '', component: RecipeListComponent},
-  {path: 'recipe',component:RecipeListComponent},
-  {path:'recipe/recipe/:id',component:RecipesDetailComponent}];
+  //{path: '', component: RecipeListComponent},
+  {path: 'recipe', component: RecipeListComponent},
+  {path: 'recipe/recipe/:id', component: RecipesDetailComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
